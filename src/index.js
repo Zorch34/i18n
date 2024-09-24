e48543d9fd1397a6f7da1a35a6211d672c783e7a
Carlos Medina <88800667+Zorch34@users.noreply.github.com>
2024-09-24 18:31:26 +0000

    I18N

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { IntlProvider } from 'react-intl';
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
 import JobsList from "./components/jobslist";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+
+const userLocale = navigator.language || navigator.userLanguage;
+
+// Seleccionar el conjunto de mensajes según el idioma del navegador
+const messages = userLocale.startsWith('es') ? localeEsMessages : localeEnMessages;
+
+ReactDOM.render(
+    <IntlProvider locale={userLocale} messages={messages}>
+        <JobsList locale={userLocale} />
+    </IntlProvider>, 
+    document.getElementById("root")
+);
